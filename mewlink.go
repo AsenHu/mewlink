d8ffee9657f2c49344e01fc73838ce133ef54931
AsenHu <78863300+AsenHu@users.noreply.github.com>
2025-03-04 09:24:35 +0000

    移动文件

--- mewlink.go
+++ mewlink.go
@@ -1,4 +1,4 @@
 package mewlink
 
 //go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go@latest
-//go:generate protoc --go_out=. --go_opt=paths=import --go_opt=module=github.com/AsenHu/mewlink ./protos/v1/roominfo.proto
+//go:generate protoc --go_out=. --go_opt=paths=import --go_opt=module=github.com/AsenHu/mewlink ./protos/roominfo.proto
